f041db69cf3fd7f7624714e898dbaff6a9b0d6fe
Rashid Umarov <rasrasras2014@mail.ru>
2024-09-11 21:13:30 +0300

    updated the page Changer: now you can replace WIDTH and HEIGHT inti tage IMG

--- changer.js
+++ changer.js
@@ -9,7 +9,7 @@ let right_list=[];
 
 const result_text=document.getElementById("check");
 const result_block=document.getElementById("replace-block");
-
+const result_img_block=document.getElementById("replace-img-block");
 
 // ищем кнопку LOOK FOR IMG
 const look_img=document.getElementById("look-img");
@@ -33,7 +33,7 @@ console.log("button replace_src = ", replace_src);
 //console.log(result_text);
 
 // читаем html текст из textarea и подсчитываем количество src в тегах img
-function getHtmlText(textarea_node, src_list) {
+function getHtmlText(textarea_node, src_list, img_list) {
     text=textarea_node.value;
 
     const parser= new DOMParser();
@@ -45,7 +45,9 @@ function getHtmlText(textarea_node, src_list) {
     let img_counter=0;
     for (const element of allElements) {
       img_counter++;
-      //console.log(element);
+      //console.log("img=",element);
+      // добавим элемент <img> в массив
+      img_list.push(element);
       const attrs = element.getAttributeNames();
       //console.log(attrs);
       for (let ind in attrs){
@@ -63,13 +65,15 @@ function getHtmlText(textarea_node, src_list) {
 
 // ищет все src в тегах IMG на обоих панелях
 function searchImgSrc(){
-    left_list=[];
-    right_list=[];
-    getHtmlText(left_editor, left_list);
-    console.log("left list:",left_list);
+    left_img_list=[];  // список найденных <img> в окне слева
+    right_img_list=[];  // список найденных <img> в окне справа
+    left_list=[];  // список  атрибутов src в найденных тегах <img> слева
+    right_list=[]; // список  атрибутов src в найденных тегах <img> справа
+    getHtmlText(left_editor, left_list, left_img_list);
+    console.log("left img list:",left_img_list);
     console.log("------------------");
-    getHtmlText(right_editor, right_list);
-    console.log("right list:",right_list);
+    getHtmlText(right_editor, right_list, right_img_list);
+    console.log("right img list:",right_img_list);
     let answer="";
     if (left_list.length>0){
         let size=Math.min(left_list.length, right_list.length);
@@ -89,6 +93,7 @@ function searchImgSrc(){
             result_text.innerHTML=answer;
             result_text.style.setProperty("display", 'block');
             result_block.style.setProperty("display", 'block');
+            result_img_block.style.setProperty("display", 'block');
         }
     }
 }
@@ -121,6 +126,53 @@ function replaceSrcInImg(){
             if(left != right){
                 element.setAttribute('src',left_list[img_counter]);
                 console.log(`replace ${right}  -> ${left}`);
+            }
+         }
+      }
+      img_counter++;
+    }
+    right_editor.value=htmlDOM.body.innerHTML;
+}
+
+function replaceImg(){
+    if (left_list.length>0){
+        let size=Math.min(left_list.length, right_list.length);
+    }
+    text=right_editor.value;
+    if (text.length==0){
+        console.log('right panel is empty');
+        return;
+    }
+    const parser= new DOMParser();
+    const htmlDOM=parser.parseFromString(text,"text/html");
+    console.log("htmlDOM => ",htmlDOM);
+    let src;
+
+    const allElements = htmlDOM.getElementsByTagName('IMG');
+    let img_counter=0;
+    for (const element of allElements) {
+      const attrs = element.getAttributeNames();
+      for (let ind in attrs){
+         let attr_name=attrs[ind];
+         // будем менять src
+         if (attr_name==='src'){
+//            console.log(`  ${img_counter}. src= ${src}:  attr_name=${attr_name}`)
+            let left=left_list[img_counter];
+            let right=element.getAttribute('src');
+            if(left != right){
+                element.setAttribute('src',left_list[img_counter]);
+                console.log(`replace ${right}  -> ${left}`);
+            }
+         }
+
+         if (attr_name==='width' || attr_name==='height'){
+//            console.log(`  ${img_counter}. src= ${src}:  attr_name=${attr_name}`)
+            let left=left_img_list[img_counter].getAttribute(attr_name);;
+            let right=element.getAttribute(attr_name);
+            //console.log(attr_name,'  left=',left,'    right=',right)
+            if(left != right) {
+                element.setAttribute(attr_name,left);
+                console.log(`replace ${attr_name} ${right}  -> ${left}`);
             }
          }
       }
